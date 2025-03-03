7f827f014dc94c5f663ff023f68938fe4b77cacf
DeveloperTanmay <umas81261@gmail.com>
2025-03-03 19:29:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Set Intersection
+
+setc1 = {"apple", "banana", "cherry", "orange"}
+setc2 = {"google", "microsoft", "apple", "cherry"}
+print("Original sets:")
+print(setc1)
+print(setc2)
+print("Intersection of two said sets:")
+setc3 = setc1.intersection(setc2)
+print(setc3)
